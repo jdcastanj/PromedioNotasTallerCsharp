2f25670e2c05c54cf4b3de9b3dfe2d5983d36e77
David Castaneda <david.castaneda@udea.edu.co>
2020-08-23 00:29:54 -0500

    Algoritmo que recibe tres notas y devuelve el promedio

---
+++ EjercicioPromedioNotasTaller/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace EjercicioPromedioNotasTaller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtnota1 = new System.Windows.Forms.TextBox();
+            this.txtnota2 = new System.Windows.Forms.TextBox();
+            this.txtnota3 = new System.Windows.Forms.TextBox();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.btnborrar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblresultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(115, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(373, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Promedio de notas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(66, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(305, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese la primera nota";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(66, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(319, 32);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ingrese la segunda nota";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(66, 199);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(297, 32);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ingrese la tercera nota";
+            // 
+            // txtnota1
+            // 
+            this.txtnota1.Location = new System.Drawing.Point(405, 90);
+            this.txtnota1.MaxLength = 4;
+            this.txtnota1.Name = "txtnota1";
+            this.txtnota1.Size = new System.Drawing.Size(100, 26);
+            this.txtnota1.TabIndex = 4;
+            // 
+            // txtnota2
+            // 
+            this.txtnota2.Location = new System.Drawing.Point(405, 142);
+            this.txtnota2.MaxLength = 4;
+            this.txtnota2.Name = "txtnota2";
+            this.txtnota2.Size = new System.Drawing.Size(100, 26);
+            this.txtnota2.TabIndex = 5;
+            // 
+            // txtnota3
+            // 
+            this.txtnota3.Location = new System.Drawing.Point(405, 199);
+            this.txtnota3.MaxLength = 4;
+            this.txtnota3.Name = "txtnota3";
+            this.txtnota3.Size = new System.Drawing.Size(100, 26);
+            this.txtnota3.TabIndex = 6;
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncalcular.Location = new System.Drawing.Point(540, 93);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(204, 132);
+            this.btncalcular.TabIndex = 7;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // btnborrar
+            // 
+            this.btnborrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnborrar.Location = new System.Drawing.Point(540, 253);
+            this.btnborrar.Name = "btnborrar";
+            this.btnborrar.Size = new System.Drawing.Size(204, 132);
+            this.btnborrar.TabIndex = 8;
+            this.btnborrar.Text = "Borrar";
+            this.btnborrar.UseVisualStyleBackColor = true;
+            this.btnborrar.Click += new System.EventHandler(this.btnborrar_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(48, 299);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(145, 32);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Promedio:";
+            this.label5.Visible = false;
+            // 
+            // lblresultado
+            // 
+            this.lblresultado.AutoSize = true;
+            this.lblresultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblresultado.Location = new System.Drawing.Point(210, 299);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(142, 32);
+            this.lblresultado.TabIndex = 10;
+            this.lblresultado.Text = "resultado";
+            this.lblresultado.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblresultado);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnborrar);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.txtnota3);
+            this.Controls.Add(this.txtnota2);
+            this.Controls.Add(this.txtnota1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtnota1;
+        private System.Windows.Forms.TextBox txtnota2;
+        private System.Windows.Forms.TextBox txtnota3;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.Button btnborrar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblresultado;
+    }
+}
+
---
+++ EjercicioPromedioNotasTaller/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjercicioPromedioNotasTaller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            double nota1, nota2, nota3, suma, resultado;
+            nota1 = double.Parse(txtnota1.Text);
+            nota2 = double.Parse(txtnota2.Text);
+            nota3 = double.Parse(txtnota3.Text);
+            suma = nota1 + nota2 + nota3;
+            resultado = suma /3;
+            lblresultado.Text = Convert.ToString(resultado);
+            label5.Visible = true;
+            lblresultado.Visible = true;
+        }
+
+        private void btnborrar_Click(object sender, EventArgs e)
+        {
+            txtnota1.Text = String.Empty;
+            txtnota2.Text = String.Empty;
+            txtnota3.Text = String.Empty;
+            txtnota1.Focus();
+            label5.Visible = false;
+            lblresultado.Visible = false;
+        }
+    }
+}
